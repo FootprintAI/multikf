8dcae5fd51ad2bfe0f5d854e7b45e474d7a1b622
hsinhoyeh <yhh92u@gmail.com>
2022-05-16 15:13:17 +0800

    feat: add docker script on linux

---
+++ docker/linux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# run as root
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+usermod -aG docker ubuntu
