67262f4ffd9beb8a2c7ef21d4b9fbf9a77a20bf7
hsinhoyeh <yhh92u@gmail.com>
2022-05-22 14:46:16 +0800

    fix(docker): fix docker installation script by disable grand default ubuntu user to docker group

--- docker/linux.sh
+++ docker/linux.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # run as root
+if (( $EUID != 0 )); then
+   echo "this script should be running as root identity"
+   exit
+fi
 
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
@@ -12,4 +16,10 @@ add-apt-repository \
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
-usermod -aG docker ubuntu
+echo "==============================="
+echo "installation completed, please add your user into docker group, something like"
+echo "****"
+echo "usermod -aG docker ubuntu"
+echo "****"
+echo "for user ubuntu"
+echo "And try to logout/login again, and see if `docker ps` works"
