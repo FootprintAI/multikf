87cc73c2d6e4c55b57dc5405d2fd6ee764aff24a
hsinhoyeh <yhh92u@gmail.com>
2022-08-09 10:58:51 +0800

    feat: upgrade kind to 0.16.0, ubuntu 2204, k8s: v1.21.9

--- pkg/machine/kubectl/cli_windows.go
+++ pkg/machine/kubectl/cli_windows.go
@@ -5,7 +5,7 @@ package kubectl
 
 var urlBinaryRes = binaryResource{
 	Os:      "windows",
-	Kind:    "https://github.com/FootprintAI/kind/raw/gpu/bin/kind-windows",
+	Kind:    "https://github.com/FootprintAI/kind/releases/download/v0.16.0-gpu-master/kind-windows",
 	Kubectl: "https://storage.googleapis.com/kubernetes-release/release/v1.21.2/bin/windows/amd64/kubectl.exe",
 }
 
