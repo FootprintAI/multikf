068f5d75ba342a598f4c971d6975503fe90adc20
hsinhoyeh <yhh92u@gmail.com>
2022-08-18 10:56:56 +0800

    fix(hack): fix install docker linux script by adding yes for the command

--- hack/docker-install/install-docker-linux.sh
+++ hack/docker-install/install-docker-linux.sh
@@ -9,7 +9,7 @@ fi
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
+add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
