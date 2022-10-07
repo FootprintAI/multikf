782746a81ae448928b2b3f457e8b4ff2c5a2faad
hsinhoyeh <yhh92u@gmail.com>
2022-10-07 10:45:38 +0800

    script(cuda): add cudnn installtion

--- hack/gpu/get-cudadriver.sh
+++ hack/gpu/get-cudadriver.sh
@@ -6,15 +6,21 @@ if (( $EUID != 0 )); then
    exit
 fi
 
+ENV OS=ubuntu2204
+ENV cudnn_version=8.6.0.*
+ENV cuda_version=cuda11.8
 
-echo " this script runs on ubuntu2004, for other version please check https://developer.nvidia.com/cuda-downloads"
+echo " this script runs on ${OS}, for other version please check https://developer.nvidia.com/cuda-downloads"
+
 
 apt-get update
 apt-get install -y wget
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+wget https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin \
+    && mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/3bf863cc.pub
+add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/ /"
+
 apt-get update
-apt-get -y install cuda
+apt-get install -y cuda libcudnn8=${cudnn_version}-1+${cuda_version} && \
+    libcudnn8-dev=${cudnn_version}-1+${cuda_version}
