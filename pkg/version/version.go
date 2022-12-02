e663e8487845d278c00c5c7f9d4b1c91bf86224b
hsinhoyeh <yhh92u@gmail.com>
2022-12-02 09:33:00 +0800

    Bump version v2.3.7

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -9,7 +9,7 @@ import (
 var (
 	BuildTime   = ""
 	GitCommitId = ""
-	version, _  = goversion.NewVersion("2.3.6")
+	version, _  = goversion.NewVersion("2.3.7")
 )
 
 func GetVersion() *goversion.Version {
