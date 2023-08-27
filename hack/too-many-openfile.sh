c8530668a3dd0cd80aad5dfcf293b754f16814f3
hsinhoyeh <yhh92u@gmail.com>
2023-08-28 06:39:53 +0800

    Update too-many-openfile.sh

--- hack/too-many-openfile.sh
+++ hack/too-many-openfile.sh
@@ -3,3 +3,9 @@
 # when running too-many-open-file issue, use
 sudo sysctl fs.inotify.max_user_instances=1280
 sudo sysctl fs.inotify.max_user_watches=655360
+
+# or modify /etc/sysctl.conf to persist a reboot
+# sudo vim /etc/sysctl.conf
+# + fs.inotify.max_user_instances=1280
+# + fs.inotify.max_user_watches=655360
+#
