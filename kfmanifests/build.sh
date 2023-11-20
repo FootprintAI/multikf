c7208d72d72d29b4a6fe36f8b84be96c1a48ad52
hsinhoyeh <yhh92u@gmail.com>
2023-11-20 16:35:31 +0800

    feat: add kubeflow v1.8.0
    fixed: #44

--- kfmanifests/build.sh
+++ kfmanifests/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-kustomize build base/kf16 > manifests/kubeflow-manifest-v1.6.1-template.yaml
-kustomize build base/kf17 > manifests/kubeflow-manifest-v1.7.0-template.yaml
+#kustomize build base/kf16 > manifests/kubeflow-manifest-v1.6.1-template.yaml
+#kustomize build base/kf17 > manifests/kubeflow-manifest-v1.7.0-template.yaml
+kustomize build base/kf18 > manifests/kubeflow-manifest-v1.8.0-template.yaml
