8fe940adc5ce7bf5f7ae6c7e699a76ffc8fb7e63
hsinatfootprintai <149764663+hsinatfootprintai@users.noreply.github.com>
2023-12-11 11:16:19 +0800

    Update 3.setup_worker_node.sh

--- hack/kubeadm/3.setup_worker_node.sh
+++ hack/kubeadm/3.setup_worker_node.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+## or run
+#
+# kubeadm token create --print-join-command
+#
+#
+
 kubeadm join $MASTERIP:8443 --token $KUBEADM_TOKEN \
     --discovery-token-unsafe-skip-ca-verification
 
