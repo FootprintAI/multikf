5a49e9a29174f170130ac3aac54b2acfef2707d5
hsinatfootprintai <149764663+hsinatfootprintai@users.noreply.github.com>
2024-03-17 13:40:12 +0800

    Update 0.install_kubeadm.sh

--- hack/kubeadm/0.install_kubeadm.sh
+++ hack/kubeadm/0.install_kubeadm.sh
@@ -4,6 +4,8 @@
 # as the package repo has changed, see https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/change-package-repository/
 # so we disable the following command, download binaries instead.
 
+apt-get install ethtool socat conntrack -y
+
 # curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 # cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 # deb https://apt.kubernetes.io/ kubernetes-xenial main
