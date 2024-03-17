89bd484a0f131ab4bf9dcd6029bb5a59016c180b
hsinatfootprintai <149764663+hsinatfootprintai@users.noreply.github.com>
2024-03-17 13:28:54 +0800

    Update 0.install_kubeadm.sh

--- hack/kubeadm/0.install_kubeadm.sh
+++ hack/kubeadm/0.install_kubeadm.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
 # install kubeadm
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y kubelet=1.25.14-00 kubeadm=1.25.14-00 kubectl=1.25.14-00
-apt-mark hold kubeadm kubelet kubectl
+# as the package repo has changed, see https://kubernetes.io/docs/tasks/administer-cluster/kubeadm/change-package-repository/
+# so we disable the following command, download binaries instead.
+
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+# cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+# deb https://apt.kubernetes.io/ kubernetes-xenial main
+# EOF
+# apt-get update
+# apt-get install -y kubelet=1.25.14-00 kubeadm=1.25.14-00 kubectl=1.25.14-00
+# apt-mark hold kubeadm kubelet kubectl
+
+curl -LO https://dl.k8s.io/release/v1.25.14/bin/linux/amd64/kubectl \
+        && chmod +x kubectl \
+        && mv kubectl /usr/bin/
+curl -LO https://dl.k8s.io/release/v1.25.14/bin/linux/amd64/kubeadm \
+        && chmod +x kubeadm \
+        && mv kubeadm /usr/bin/
+curl -LO https://dl.k8s.io/release/v1.25.14/bin/linux/amd64/kubelet \
+        && chmod +x kubelet \
+        && mv kubelet /usr/bin/
 
 # if encountered the following error during install kubelet
 # W: An error occurred during the signature verification. The repository is not updated and the previous index files will be used. GPG error: https://packages.cloud.google.com/apt kubernetes-xenial InRelease: NO_PUBKEY B53DC80D13EDEF05
