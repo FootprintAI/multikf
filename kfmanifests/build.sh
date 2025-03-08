cfcd81d48928a7e3ed9d365784b95091d60f5205
hsinhoyeh <yhh92u@gmail.com>
2025-03-08 10:33:11 +0800

    feat(kubeflow191): add kubeflow v1.9.1

--- kfmanifests/build.sh
+++ kfmanifests/build.sh
@@ -3,4 +3,4 @@
 #kustomize build base/kf16 > manifests/kubeflow-manifest-v1.6.1-template.yaml
 #kustomize build base/kf17 > manifests/kubeflow-manifest-v1.7.0-template.yaml
 #kustomize build base/kf18 > manifests/kubeflow-manifest-v1.8.1-template.yaml
-kustomize build base/kf19 > manifests/kubeflow-manifest-v1.9.0-template.yaml
+kustomize build base/kf19 > manifests/kubeflow-manifest-v1.9.1-template.yaml
