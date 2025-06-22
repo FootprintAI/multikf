22b39364c36f110231b2999731a503f24179bdda
hsinatfootprintai <149764663+hsinatfootprintai@users.noreply.github.com>
2025-06-23 06:13:09 +0800

    Update 0.prerequsite.sh

--- hack/kubeadm/0.prerequsite.sh
+++ hack/kubeadm/0.prerequsite.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+sudo modprobe br_netfilter
+
+# 設定讓每次開機自動載入
+echo 'br_netfilter' | sudo tee /etc/modules-load.d/br_netfilter.conf
+
 # allow iptable see bridged traffic
 cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
 sysctl --system
