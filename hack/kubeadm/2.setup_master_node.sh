a2ac647c3f371a49e52c40ad92c7de02f6672428
hsinatfootprintai <149764663+hsinatfootprintai@users.noreply.github.com>
2025-06-23 06:16:31 +0800

    Update 2.setup_master_node.sh
    add cilium script

--- hack/kubeadm/2.setup_master_node.sh
+++ hack/kubeadm/2.setup_master_node.sh
@@ -25,3 +25,33 @@ kubectl taint nodes <node-name> node-role.kubernetes.io/control-plane-
 ## taint master
 # kubectl taint nodes <node-name> node-role.kubernetes.io/master:NoSchedule
 # kubectl taint nodes <node-name> node-role.kubernetes.io/control-plane:NoSchedule
+
+
+
+##### cilium
+
+kubeadm init --apiserver-bind-port=8443 \
+  --apiserver-advertise-address=$PUBLICIP \
+  --kubernetes-version=$KUBECTL_VERSION \
+  --pod-network-cidr=10.0.0.0/16 \  # Recommended default for Cilium
+  --service-cidr=10.96.0.0/12 \
+  --token=$KUBEADM_TOKEN \
+  --ignore-preflight-errors=NumCPU \
+  --apiserver-cert-extra-sans="$LOCALHOST,$PUBLICIP"
+
+# Setup kubectl config
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+# Install Cilium (via CLI installer)
+curl -L --remote-name https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz
+tar xzvf cilium-linux-amd64.tar.gz
+sudo mv cilium /usr/local/bin/
+
+# Install Cilium into the cluster
+cilium install
+
+# Optionally verify installation
+cilium status --wait
